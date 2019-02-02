d9c9c2e899b73bed8493fd3539d8a415eaf509f5
wangkun <kunwang1@jieyuechina.com>
2019-02-02 15:18:49 +0800

    trim demo sql and h2 config file

--- src/main/resources/h2-demo/h2.sql
+++ src/main/resources/h2-demo/h2.sql
@@ -1,12 +1,17 @@
+
+drop table if exists dashboard_board;
 CREATE TABLE dashboard_board (
   board_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(50) NOT NULL,
   category_id bigint(20) DEFAULT NULL,
   board_name varchar(100) NOT NULL,
   layout_json text,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
   PRIMARY KEY (board_id)
 );
 
+drop table if exists dashboard_category;
 CREATE TABLE dashboard_category (
   category_id bigint(20) NOT NULL AUTO_INCREMENT,
   category_name varchar(100) NOT NULL,
@@ -14,33 +19,43 @@ CREATE TABLE dashboard_category (
   PRIMARY KEY (category_id)
 );
 
+drop table if exists dashboard_datasource;
 CREATE TABLE dashboard_datasource (
   datasource_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(50) NOT NULL,
   source_name varchar(100) NOT NULL,
   source_type varchar(100) NOT NULL,
   config text,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
   PRIMARY KEY (datasource_id)
 );
 
+drop table if exists dashboard_widget;
 CREATE TABLE dashboard_widget (
   widget_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(100) NOT NULL,
   category_name varchar(100) DEFAULT NULL,
   widget_name varchar(100) DEFAULT NULL,
   data_json text,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
   PRIMARY KEY (widget_id)
 );
 
+drop table if exists dashboard_dataset;
 CREATE TABLE dashboard_dataset (
   dataset_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(100) NOT NULL,
   category_name varchar(100) DEFAULT NULL,
   dataset_name varchar(100) DEFAULT NULL,
   data_json text,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
   PRIMARY KEY (dataset_id)
 );
 
+drop table if exists dashboard_user;
 CREATE TABLE dashboard_user (
   user_id varchar(50) NOT NULL,
   login_name varchar(100) DEFAULT NULL,
@@ -53,6 +68,7 @@ CREATE TABLE dashboard_user (
 INSERT INTO dashboard_user (user_id,login_name,user_name,user_password)
 VALUES('1', 'admin', 'Administrator', 'ff9830c42660c1dd1942844f8069b74a');
 
+drop table if exists dashboard_user_role;
 CREATE TABLE dashboard_user_role (
   user_role_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(100) DEFAULT NULL,
@@ -60,6 +76,7 @@ CREATE TABLE dashboard_user_role (
   PRIMARY KEY (user_role_id)
 );
 
+drop table if exists dashboard_role;
 CREATE TABLE dashboard_role (
   role_id varchar(100) NOT NULL,
   role_name varchar(100) DEFAULT NULL,
@@ -67,6 +84,7 @@ CREATE TABLE dashboard_role (
   PRIMARY KEY (role_id)
 );
 
+drop table if exists dashboard_role_res;
 CREATE TABLE dashboard_role_res (
   role_res_id bigint(20) NOT NULL AUTO_INCREMENT,
   role_id varchar(100) DEFAULT NULL,
@@ -76,6 +94,8 @@ CREATE TABLE dashboard_role_res (
   PRIMARY KEY (role_res_id)
 );
 
+
+drop table if exists dashboard_job;
 CREATE TABLE dashboard_job (
   job_id bigint(20) NOT NULL AUTO_INCREMENT,
   job_name varchar(200) DEFAULT NULL,
@@ -91,23 +111,41 @@ CREATE TABLE dashboard_job (
   PRIMARY KEY (job_id)
 );
 
+drop table if exists dashboard_board_param;
 CREATE TABLE dashboard_board_param (
   board_param_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(50) NOT NULL,
   board_id bigint(20) NOT NULL,
   config text,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
   PRIMARY KEY (board_param_id)
 );
 
--- 升级0.4需要执行的
-ALTER  TABLE  dashboard_dataset ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_dataset ADD update_time TIMESTAMP DEFAULT now();
 
-ALTER  TABLE  dashboard_datasource ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_datasource ADD update_time TIMESTAMP DEFAULT now();
 
-ALTER  TABLE  dashboard_widget ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_widget ADD update_time TIMESTAMP DEFAULT now();
+drop table if exists dashboard_homepage;
+CREATE TABLE dashboard_homepage (
+  board_id bigint(20) NOT NULL,
+  user_id varchar(50) NOT NULL,
+  PRIMARY KEY (board_id, user_id)
+);
 
-ALTER  TABLE  dashboard_board ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_board ADD update_time TIMESTAMP DEFAULT now();
+drop table if exists sales_fact_sample_flat;
+CREATE TABLE `sales_fact_sample_flat` (
+  `the_year` int(7) DEFAULT NULL,
+  `month_of_year` smallint(6) DEFAULT NULL,
+  `day_of_month` smallint(6) DEFAULT NULL,
+  `the_date` datetime DEFAULT NULL,
+  `SALES_DISTRICT` varchar(30)  DEFAULT NULL,
+  `SALES_REGION` varchar(30)  DEFAULT NULL,
+  `SALES_COUNTRY` varchar(30)  DEFAULT NULL,
+  `yearly_income` varchar(30)  NOT NULL,
+  `total_children` smallint(6) NOT NULL,
+  `member_card` varchar(30)  DEFAULT NULL,
+  `num_cars_owned` int(11) DEFAULT NULL,
+  `gender` varchar(30)  NOT NULL,
+  `store_sales` decimal(10,4) NOT NULL,
+  `store_cost` decimal(10,4) NOT NULL,
+  `unit_sales` decimal(10,4) NOT NULL
+) DEFAULT CHARSET=utf8;
